296877ea4b0ce7993e056e01151bb917e62bfa5e
tokushigeshuya <narihare1130@gmail.com>
2022-04-14 16:49:37 +0000

    [add]layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery
+//= require rails-ujs
+
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -16,3 +20,9 @@ import "../stylesheets/application"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(function() {
+  $('.slider').slick({
+      dots: true,
+  });
+})
