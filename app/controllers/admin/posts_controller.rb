4a499c0af538616233aa0c0b5eacf065d5959137
tokushigeshuya <narihare1130@gmail.com>
2022-04-17 15:29:15 +0000

    [add]admin_post_show

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,21 @@
 class Admin::PostsController < ApplicationController
+  def index
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:body,:image,:address,:latitude,:longitude)
+  end
 end
